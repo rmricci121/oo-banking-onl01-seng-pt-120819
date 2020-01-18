9b3c792381db812d068ad595eb47015ec9925b92
Robert M Ricci <rmricci121@gmail.com>
2020-01-18 14:24:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,11 +10,8 @@ class Transfer
   end
 
   def valid?
-    if sender.valid? && receiver.valid?
-      true 
-    else
-      false
-    end
+   sender.valid?
+   receiver.valid?
   end
 
   def execute_transaction
